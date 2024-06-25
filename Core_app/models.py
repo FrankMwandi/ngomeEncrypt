6fa77999e4d8c9d2e51e6b2daa2db06a639f7238
mwandi <frankmwandi93@gmail.com>
2024-06-25 18:45:49 +0300

    Encryption Log model

--- Core_app/models.py
+++ Core_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class EncryptionLog(models.Model):
+    ip_address = models.CharField(max_length=45)
+    filename = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.filename} from {self.ip_address} at {self.timestamp}"
