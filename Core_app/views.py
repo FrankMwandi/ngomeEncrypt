6fb3ab0a31dce5ade3c63f56c3ea7edf816a6965
mwandi <frankmwandi93@gmail.com>
2024-06-25 18:43:08 +0300

    upload file view

--- Core_app/views.py
+++ Core_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import UploadFileForm
+from .utils import encrypt_file
+from .models import EncryptionLog
 
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            password = form.cleaned_data['password']
+            encrypted_data = encrypt_file(file, password)
+            
+            # Log the usage
+            EncryptionLog.objects.create(
+                ip_address=request.META.get('REMOTE_ADDR'),
+                filename=file.name
+            )
+
+            response = HttpResponse(encrypted_data['ciphertext'], content_type='application/octet-stream')
+            response['Content-Disposition'] = f'attachment; filename="{file.name}.enc"'
+            return response
+    else:
+        form = UploadFileForm()
+    return render(request, 'encryption_app/upload.html', {'form': form})
+
