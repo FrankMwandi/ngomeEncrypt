d25432bd5f93b6743aaeebe536178eb2eee21318
mwandi <frankmwandi93@gmail.com>
2024-06-25 23:17:43 +0300

    adding enc or dcr option in the log model

--- Core_app/models.py
+++ Core_app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class EncryptionLog(models.Model):
     ip_address = models.CharField(max_length=45)
     filename = models.CharField(max_length=255)
+    action = models.CharField(max_length=10)  # 'encrypt' or 'decrypt'
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.filename} from {self.ip_address} at {self.timestamp}"
+        return f"{self.filename} ({self.action}) from {self.ip_address} at {self.timestamp}"
