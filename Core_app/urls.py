1434025699c84fdacfde0b74d88bfc09a4d14e44
mwandi <frankmwandi93@gmail.com>
2024-06-26 16:55:59 +0300

    home url

--- Core_app/urls.py
+++ Core_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_file, name='upload_file'),
+    path('', views.home, name='home'),
+    path('encrypt/', views.upload_file, name='upload_file'),
     path('decrypt/', views.decrypt_file_view, name='decrypt_file'),
 ]
