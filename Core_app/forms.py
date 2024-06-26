657cd97de385da67dd357bc10018aaac69f9aa7a
mwandi <frankmwandi93@gmail.com>
2024-06-26 11:48:23 +0300

    update views

--- Core_app/forms.py
+++ Core_app/forms.py
@@ -3,3 +3,7 @@ from django import forms
 class UploadFileForm(forms.Form):
     file = forms.FileField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class DecryptFileForm(forms.Form):
+    encrypted_file = forms.FileField()
+    password = forms.CharField(widget=forms.PasswordInput)
